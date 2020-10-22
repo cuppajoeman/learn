62e40e192c5fbe466fef235029d2bc1530e0f154
ccn <ccn@cuppajoeman.com>
2020-10-21 22:45:39 -0400

    add in something custom

--- web-react/src/components/UserCount.js
+++ web-react/src/components/UserCount.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 import Title from './Title'
-import { useQuery, gql } from '@apollo/client'
+import { useQuery, gql, useMutation } from '@apollo/client'
 
 const useStyles = makeStyles({
   depositContext: {
@@ -20,10 +20,19 @@ const GET_COUNT_QUERY = gql`
   }
 `
 
+const CREATE_AOS = gql`
+    mutation CREATE_AOS($title: String) {
+      createAreaOfStudy(title: $title) {
+        title
+      }
+    }
+`;
+
 export default function Deposits() {
   const classes = useStyles()
 
   const { loading, error, data } = useQuery(GET_COUNT_QUERY)
+  const { AOS, title } = useMutation(CREATE_AOS)
   if (error) return <p>Error</p>
   return (
     <React.Fragment>
@@ -39,6 +48,7 @@ export default function Deposits() {
           View users
         </Link>
       </div>
+      <button onclick= "AOS({ variables: { title: 'Mathematix'}})">plz fuxing work</button>
     </React.Fragment>
   )
 }
